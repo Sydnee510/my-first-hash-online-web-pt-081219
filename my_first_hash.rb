8cf803ce8c33eadb87390f56e1414f507fcda14c
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-08-23 22:33:24 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  my_hash = {hats => brown}
+  new = {hats => brown}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
